7237da61d2185450f1f5a28b8954561480e02104
Frankie Fisher <frankie@terrorise.me.uk>
2026-08-05 06:33:44 +0100

    chore: update example-screeps.toml, Cargo.toml and src/lib.rs with season3 arenas

--- src/lib.rs
+++ src/lib.rs
@@ -42,9 +42,9 @@ impl Bot {
         let info = game::arena_info();
         warn!("arena_info: {:?}", info);
 
-        // strategy for spawn and swamp arena, which will conditionally compile in
+        // strategy for spawn strike arena, which will conditionally compile in
         // only when this feature is enabled for the crate
-        #[cfg(feature = "arena-spawn-and-swamp")]
+        #[cfg(feature = "season3-spawn_strike")]
         {
             let mut enemy_spawn = None;
             let spawns = game::utils::get_objects_by_prototype(prototypes::STRUCTURE_SPAWN);
